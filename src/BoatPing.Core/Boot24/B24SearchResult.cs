e0110c6aefdee74b7dc401a4f896f9f2731b4760
meerownymous <meerow@mirmx.de>
2020-12-26 13:56:41 +0100

    ci: init

---
+++ src/BoatPing.Core/Boot24/B24SearchResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+using Yaapii.Atoms.Scalar;
+using Yaapii.Xml;
+
+namespace BoatPing.Core.Boot24
+{
+    /// <summary>
+    /// Search result from a Boot24.de search.
+    /// </summary>
+    public class B24SearchResult : XMLEnvelope
+    {
+        public B24SearchResult() : base(new ScalarOf<IXML>(() => new XMLCursor("<boats><boat>bob</boat></boats>")))
+        {
+        }
+    }
+
+    public sealed class B24SearchResultTests
+    {
+        [Fact]
+        public void Works()
+        {
+            throw new Exception("Doesnt");
+        }
+    }
+}
