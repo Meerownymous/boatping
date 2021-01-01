a4cdcc7e227d4f3a7ba6336fd992b0426fcc3d6d
meerownymous <meerow@mirmx.de>
2021-01-01 16:26:27 +0100

    * TgmNotifications.cs:
    * Program.cs: feat: scrape boot24.de for Netherlands

--- BoatPing.Run/Program.cs
+++ BoatPing.Run/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using BoatPing.Core;
 using BoatPing.Core.Boot24;
 using BoatPing.Core.Notification;
+using Yaapii.Atoms.Enumerable;
 
 namespace BoatPing.Run
 {
@@ -9,27 +12,66 @@ namespace BoatPing.Run
     {
         static void Main(string[] args)
         {
+            var path = Environment.CurrentDirectory;
+            var logbook = new BoatPing.Core.LogBook.FileLogBook(Path.Combine(path, "memory"));
+            var fileNotifications = new Core.FileNotifications(Path.Combine(path, "memory"));
+            var telegramNotifications = new Core.Notification.Telegram.TgmNotifications();
 
             while (true)
             {
-                var path = Environment.CurrentDirectory;
-                var logbook = new BoatPing.Core.LogBook.FileLogBook(Path.Combine(path, "memory"));
-                var notifications = new Core.FileNotifications(Path.Combine(path, "memory"));
+                
 
-                foreach (var ad in new B24Ads(new B24DefaultSearch()))
+                try
                 {
-                    try
+                    var ads = 0;
+                    var stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    foreach (var ad in
+                        new Joined<IAd>(
+                            new B24Ads(new B24DefaultSearch()),
+                            new B24Ads("https://www.boot24.com/segelboot/#pab=30000&pbis=58000&jahrvon=1970&lmin=10&lmax=13&land=c25")
+                        )
+                    )
                     {
-                        if (!logbook.Contains(ad))
+                        ads++;
+                        try
                         {
-                            notifications.Post(new NewBoatNotification(ad));
-                            logbook.Record(ad);
+                            if (!logbook.Contains(ad))
+                            {
+                                telegramNotifications.Post(new NewBoatNotification(ad));
+                                fileNotifications.Post(new NewBoatNotification(ad));
+                                logbook.Record(ad);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            File.AppendAllLines(
+                                Path.Combine(path, "memory", "error.log"),
+                                new ManyOf(
+                                    $"error scraping {ad.Url().ToString()}",
+                                    ex.ToString()
+                                )
+                            );
                         }
                     }
-                    catch(Exception ex)
-                    {
+                    stopwatch.Stop();
+                    File.AppendAllLines(
+                        Path.Combine(path, "memory", "scrapes.log"),
+                        new ManyOf(
+                            $"{DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss")}[] Scraped {ads} ads of {new B24DefaultSearch().AsString()} in {stopwatch.Elapsed.TotalSeconds}s"
+                        )
+                    );
 
-                    }
+                }
+                catch (Exception ex)
+                {
+                    File.AppendAllLines(
+                        Path.Combine(path, "memory", "error.log"),
+                        new ManyOf(
+                            "error scraping boot24",
+                            ex.ToString()
+                        )
+                    );
                 }
                 System.Threading.Thread.Sleep(new TimeSpan(0, 30, 0));
             }
---
+++ src/BoatPing.Core/Notification/Telegram/TgmNotifications.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace BoatPing.Core.Notification.Telegram
+{
+    /// <summary>
+    /// Notifications over Telegram.
+    /// </summary>
+    public sealed class TgmNotifications : INotifications
+    {
+        /// <summary>
+        /// Notifications over Telegram.
+        /// </summary>
+        public TgmNotifications()
+        { }
+
+        public void Post(INotification notification)
+        {
+            var botClient = new TelegramBotClient("1468952780:AAFSSKQ6gTulFkVuUyYJ7TX_Ra8Uo51La0E");
+            var updates = botClient.GetUpdatesAsync().Result; //fetch to allow sending
+
+            botClient.SendTextMessageAsync(
+                new global::Telegram.Bot.Types.ChatId(-414451065),
+                new NewBoatText(notification.Ad()).AsString()
+            );
+        }
+    }
+}
